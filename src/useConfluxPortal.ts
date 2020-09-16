1d4ece8204e1ea8168ef65a83ddf6e777545ba55
yqrashawn <namy.19@gmail.com>
2020-09-16 16:04:01 +0800

    feat(useConfluxPortal): add balance support

--- src/useConfluxPortal.ts
+++ src/useConfluxPortal.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useInterval } from './';
+import SINGLE_CALL_BALANCES_ABI from './cfx-single-call-balance-checker-abi';
 
 function openHomePage() {
   window.open('https://portal.conflux-chain.org');
@@ -9,12 +10,36 @@ function validAddresses(addresses) {
   return Array.isArray(addresses) && addresses.length;
 }
 
-function useConfluxPortal() {
+let singleCallBalanceContract =
+  window.conflux &&
+  window.conflux.isConfluxPortal &&
+  window.confluxJS.Contract({ abi: SINGLE_CALL_BALANCES_ABI, address: '0x8f35930629fce5b5cf4cd762e71006045bfeb24d' });
+
+function getTokensBalance(address, tokenAddresses) {
+  return singleCallBalanceContract
+    .balances([address], ['0x0000000000000000000000000000000000000000', ...tokenAddresses])
+    .call();
+}
+
+function getBalance(address) {
+  return window.conflux.send({ method: 'cfx_getBalance', params: [address, 'latest_state'] }, []);
+}
+
+function useConfluxPortal(tokenAddresses = [], interval = 0) {
   const portalInstalled = window.conflux && window.conflux.isConfluxPortal;
-  if (portalInstalled) window.conflux.autoRefreshOnNetworkChange = false;
-  console.log(window.conflux.autoRefreshOnNetworkChange);
+  if (portalInstalled) {
+    window.conflux.autoRefreshOnNetworkChange = false;
+    singleCallBalanceContract =
+      singleCallBalanceContract ||
+      window.confluxJS.Contract({
+        abi: SINGLE_CALL_BALANCES_ABI,
+        address: '0x8f35930629fce5b5cf4cd762e71006045bfeb24d',
+      });
+  }
   const [address, setAddress] = useState(portalInstalled && window.conflux.selectedAddress);
   const [chainId, setChainId] = useState(portalInstalled && window.conflux.chainId);
+  const [[balance, ...tokensBalance], setTokensBalance] = useState([undefined, []]);
+
   const login = () => {
     if (portalInstalled) {
       window.conflux.enable().then((addresses) => validAddresses(addresses) && setAddress(addresses[0]));
@@ -27,6 +52,16 @@ function useConfluxPortal() {
     },
     portalInstalled && chainId && chainId !== 'loading' ? null : 100
   );
+
+  useInterval(
+    () => getTokensBalance(address, tokenAddresses).then(setTokensBalance),
+    address && singleCallBalanceContract && interval ? interval : null
+  );
+
+  useEffect(() => {
+    if (!interval && address && singleCallBalanceContract)
+      getTokensBalance(address, tokenAddresses).then(setTokensBalance);
+  }, [address, JSON.stringify(tokenAddresses)]);
 
   useEffect(() => {
     if (portalInstalled) {
@@ -51,7 +86,14 @@ function useConfluxPortal() {
     }
   }, [portalInstalled]);
 
-  return [Boolean(portalInstalled), address, chainId, login, portalInstalled ? window.conflux : null];
+  return [
+    Boolean(portalInstalled),
+    address,
+    [balance, tokensBalance],
+    chainId,
+    login,
+    portalInstalled ? [window.conflux, window.confluxJS] : [null, null],
+  ];
 }
 
 useConfluxPortal.openHomePage = openHomePage;
--- stories/useConfluxPortal.story.tsx
+++ stories/useConfluxPortal.story.tsx
@@ -4,14 +4,29 @@ import { useConfluxPortal } from '../src';
 // import ShowDocs from './util/ShowDocs';
 
 const Demo = () => {
-  const [portalInstalled, address, chainId, login, conflux] = useConfluxPortal();
+  // addreess: user address
+  // balance: cfx balance
+  // token balance: same order as input token list (1st arg), will be empty array if no token list provided
+  // conflux: portal api
+  // confluxJS: js-conflux-sdk instance with portal as rpc provider
+  // login: request user portal authrorization
+  const [portalInstalled, address, [balance, tokensBalance], chainId, login, [conflux, confluxJS]] = useConfluxPortal(
+    [
+      '0x87010faf5964d67ed070bc4b8dcafa1e1adc0997', // token addreess
+      '0x8f50e31a4e3201b2f7aa720b3754dfa585b4dbfa',
+      '0x8d0ff27dbdb98f40530cc213d78d0665d5e5893a',
+    ],
+    1000 // interval of refreshing balance, no interval if pass 0 or leave undefined
+  );
   const s = () => {
-    conflux.send({ method: 'cfx_accounts' }).then(console.log);
+    conflux.send({ method: 'cfx_getBalance' }).then(console.log);
   };
   return (
     <div>
       <strong>Portal Installed</strong>:&nbsp;&nbsp; <span>{JSON.stringify(portalInstalled)}</span> <br />
       <strong>User account</strong>:&nbsp;&nbsp; <span>{JSON.stringify(address)}</span> <br />
+      <strong>Balance</strong>:&nbsp;&nbsp; <span>{JSON.stringify(balance)}</span> <br />
+      <strong>Tokens Balance</strong>:&nbsp;&nbsp; <span>{JSON.stringify(tokensBalance)}</span> <br />
       <strong>ChainId</strong>:&nbsp;&nbsp; <span>{JSON.stringify(chainId)}</span> <br />
       <button onClick={useConfluxPortal.openHomePage}>Open Portal Homepage</button>
       <button onClick={login}>Login</button>
